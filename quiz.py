889fa1ae1ca595561d751b4b0991971fb5b5ff37
millejar <56222957+millejar@users.noreply.github.com>
2019-11-04 20:44:14 -0600

    Added functionality
    Added ability to test based on a picture and ask for an artists name and the name of a piece of art. Added ability to accept different names for an answer. Further modularized the program.

--- quiz.py
+++ quiz.py
@@ -9,12 +9,13 @@ try:
     xl_workbook = xlrd.open_workbook(excel_file)
 except: 
     print("Could not open excel file")
+
 # Retrieve and print the sheetnames
 sheet_names = xl_workbook.sheet_names()
-print('Sheet Names', sheet_names)
+sheet_number = helper_functions.get_sheet(sheet_names)
 
 # You can either open a sheet by name or by index
-xl_sheet = xl_workbook.sheet_by_name('Sheet1')
+xl_sheet = xl_workbook.sheet_by_index(sheet_number)
 
 # Print the first column
 first_column = xl_sheet.col(0)
@@ -25,27 +26,24 @@ print(Fore.CYAN + Style.BRIGHT + "Welcome to the quiz!\n" + Style.RESET_ALL)
 user_continue = True
 # Develop the list of available rows 
 available_numbers = []
-for number in range(0, xl_sheet.nrows, 1):
+for number in range(1, xl_sheet.nrows):
     available_numbers.append(number)
-print(available_numbers)
 while (user_continue == True): 
     print("\n ******* \n")
     # Pick a random row from available_numbers, then remove it
-    random_row = random.choice(available_numbers)
-    available_numbers.remove(random_row)
+    row_number = random.choice(available_numbers)
+    available_numbers.remove(row_number)
     # Print the correct answer
-    correct_answer = xl_sheet.cell_value(random_row, 0)
-    #print("The correct answer is: ", correct_answer)
-    # Print the clue
-    print(Fore.CYAN + "Here is the description:" + Fore.RESET)
-    print(xl_sheet.cell_value(random_row, 1))
+    correct_answer = xl_sheet.cell_value(row_number, 0)
+    # Print the description
+    helper_functions.give_description(xl_sheet.row_values(row_number))
     # Ask for the answer 
-    response = helper_functions.user_answer(correct_answer, random_row, available_numbers)
+    response = helper_functions.test_user(xl_sheet.row_values(row_number), row_number, available_numbers)
     available_numbers = response[0]
     user_continue = response[1]
     # If all clues have been used up, finish the game, else ask the user if they wish to continue
     if (available_numbers == []):
-        print("You've answered every clue! Congratulations!")
+        print(Fore.YELLOW + "You've answered every clue! Congratulations!" + Fore.RESET)
         break
     if (user_continue == False):
         break
